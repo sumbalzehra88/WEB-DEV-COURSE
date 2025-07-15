1fb1d37b6b1e99828583f5fb1852f8bbe94503bb
Sumbal Zehra <149603065+sumbalzehra88@users.noreply.github.com>
2025-07-15 15:03:42 +0500

    commit

---
+++ script2.js
@@ -0,0 +1,26 @@
+console.log("Hey we are using java")
+let a = 5;
+let b = 6;
+let c = "code with me";
+console.log(a + b + 8);
+console.log(typeof a, typeof b, typeof c);
+{
+    let a = 10;
+    console.log(a);
+}
+console.log(a); 
+
+let d = 20;
+let e = 30;
+let f = 40;
+const g = 50;
+console.log(typeof d, typeof e, typeof f, typeof g);
+
+let o ={
+    name: "John",
+    age: 30,
+    isStudent: false
+
+};
+console.log(o);
+console.log(o.name, o.age, o.isStudent);
